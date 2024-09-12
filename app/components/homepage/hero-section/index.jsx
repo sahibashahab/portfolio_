f7db46d5d25ecd9cc442c667de477ce3a5b39686
sahibashahab <92646376+sahibashahab@users.noreply.github.com>
2024-09-12 06:16:28 -0700

    commit

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -22,12 +22,13 @@ function HeroSection() {
       <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-12 gap-y-8 items-center">
         {/* Left Section - Text Content */}
         <div className="flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+        <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hello, <br />
-            This is{' '}
-            <span className="text-pink-500">{personalData.name}</span>, I'm a{' '}
-            <span className="text-[#16f2b3]">{personalData.designation}</span>.
+            This is
+            <span className="text-pink-500"> {personalData.name}</span>, I'm a
+            <span className="text-[#16f2b3]"> {personalData.designation}</span>.
           </h1>
+
 
           <div className="my-12 flex items-center gap-5">
             <Link
