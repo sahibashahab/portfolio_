d38506520fc0c72cca73c15db37af93a18324c9d
sahiba shahab <92646376+sahibashahab@users.noreply.github.com>
2025-07-07 03:24:35 -0700

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,7 +1,16 @@
 export const projectsData = [
-    {
+     {
         
         id: 1,
+        name: "chat app",
+        description: " The chatbot uses a LLM to understand the user's messages and reply with meaningful responses. The goal is to build a helpful and intelligent chatbot that can be used for basic conversation and assistance.",
+        tools: ['react', 'python', 'openai', 'llm', 'groq'],
+        role: 'Developer',
+        
+    },
+    {
+        
+        id: 2,
         name: "Animal Recognition",
         description: "An intuitive GUI application for classifying animals in images using a deep learning model. Users can upload images, and the app predicts the type of animal and provides a confidence level. The interface includes a progress bar to visually represent the prediction confidence.",
         tools: ['Tkinter', 'TensorFlow', 'Keras', 'Pillow', 'NumPy'],
@@ -9,7 +18,7 @@ export const projectsData = [
         
     },
     {
-        id: 2,
+        id: 3,
         name: ' Face Detection',
         description: "A real-time emotion detection system that utilizes a webcam feed to detect faces and predict emotions using a pre-trained deep learning model. The system recognizes emotions such as angry, disgust, fear, happy, neutral, sad, and surprise and displays them on the screen along with bounding boxes around the detected faces.",
         tools: ['OpenCV', 'MediaPipe', 'Keras', 'TensorFlow', 'NumPy', 'Python'],
@@ -17,14 +26,14 @@ export const projectsData = [
     
     },
     {
-        id: 3,
+        id: 4,
         name: "Body language",
         description: "A real-time system that detects facial landmarks, hand gestures, and body poses using OpenCV and MediaPipe Holistic. The model predicts body language and displays the classification and its probability.",
         tools: ["OpenCV", "MediaPipe Holistic", "Python", "Scikit-learn"],
         role: "Developer",
     },
     {
-        id: 4,
+        id: 5,
         name: "Health Insurance Prediction",
         description: "A machine learning model that predicts health insurance costs based on user details such as age, BMI, smoking status, and region. The system processes data from a CSV file, performs feature engineering, and applies regression models for cost estimation.",
         tools: ["Python", "Pandas", "Scikit-learn", "Matplotlib", "Seaborn"],
@@ -32,7 +41,7 @@ export const projectsData = [
     },
     
     {
-        id: 5,
+        id: 6,
         name: "Library Management System",
         description: "A Library Management System built using C# for backend logic and an HTML/CSS frontend. The system allows users to manage books, issue/return records, and maintain user accounts. Features include book search, user authentication, and book inventory management.",
         tools: [
@@ -48,7 +57,7 @@ export const projectsData = [
         
     },
     {
-        id: 6,
+        id: 7,
         name: "Hospital Management System",
         description: "A Hospital Management System built using C# that incorporates Business Logic Layer (BLL) and Data Access Layer (DAL) architecture. The system allows management of patient records, doctor scheduling, billing, and appointment booking. It includes features such as patient history tracking, user authentication, and billing automation.",
         tools: [
