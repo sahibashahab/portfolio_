d39955016091a99232563f87a652b60586e85864
sahiba shahab <92646376+sahibashahab@users.noreply.github.com>
2025-07-16 04:17:34 -0700

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,19 +1,32 @@
 export const experiences = [
+
   {
     id: 1,
+    title: "Front-End Developer",
+    company: "rayyanco Business system  ",
+    duration: "(June 2025 continue)"
+  },
+  {
+    id: 2,
+    title: "Python Programmming interns",
+    company: "CodeX Computing Co. ",
+    duration: "(March 2025 – June 2025)"
+  },
+  {
+    id: 3,
     title: 'web development',
     company: "IDA solution digital markiting agency",
     duration: "(2022-2023)"
 
   },
   {
-    id: 2,
+    id: 4,
     title: 'Machine learning interns',
     company: "Prodigy Infotech ",
     duration: "1st April 2024 - 30 April 2024"
   },
   {
-    id: 3,
+    id: 5,
     title: "FrontEnd development interns",
     company: "Digital EmpowermentNetwork ",
     duration: "(1st Aug 2024 - 30 Aug 2024)"
